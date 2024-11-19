d0531663cdcb21b1168cd8c4d152fbe0323e5bc4
takuya44 <shinntaku2345@gmail.com>
2024-11-19 23:07:29 +0900

    station10-13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,13 @@
 function changeBackgroundColor(e) {
-  // ここに背景色を変える処理を書く。
+  // テキスト要素を取得
+  const textElement = document.getElementById("text");
 
+  // トグルボタンの状態をチェック
+  if (e.target.checked) {
+    // トグルボタンがオンのとき
+    textElement.style.backgroundColor = "red"; // 背景色を赤色に変更
+  } else {
+    // トグルボタンがオフのとき
+    textElement.style.backgroundColor = "white"; // 背景色を白色に変更
+  }
 }
